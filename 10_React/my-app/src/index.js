e68256b6c82a097154d83022694c9b2c6d878cb7
wonahyeon <wah10220@gmail.com>
2023-05-26 19:01:51 +0900

    React
    useEffect  정리
    이벤트 처리
    조건부 렌더링
    + snippets 사용법

--- 10_React/my-app/src/index.js
+++ 10_React/my-app/src/index.js
@@ -13,6 +13,14 @@ import NotificationList from './chapter6/NotificationList';
 import Counter from './chapter7/7.2/Counter';
 import SetStateMerge from './chapter7/7.2/SetStateMerge';
 import CounterEffect from './chapter7/7.3/ex1/CounterEffect';
+import EffectSummary from './chapter7/7.3/ex3/EffectSummary';
+import EffectContainer from './chapter7/7.3/ex3/EffectContainer';
+import TimerContainer from './chapter7/7.3/ex4/TimerContainer';
+import Toggle from './chapter8/8.1/Toggle';
+import MyButton from './chapter8/8.2/MyButton';
+import ConfirmButton from './chapter8/ComfirmButton';
+import Greeting from './chapter9/9.1/Greeting';
+import LoginControl from './chapter9/9.2/LoginControl';
 // import Clock from './chapter4/clock'; // Clock 이 아니라 clock으로 오류
 // TODO:툴 다시 껐다가 키면 해결
 
@@ -20,15 +28,37 @@ import CounterEffect from './chapter7/7.3/ex1/CounterEffect';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 // Root DOM Node에 렌더링 하도록 하는 함수
 // 처음으로 렌러딩할 컴포넌트를 지정하는데 App 컴포넌트가 기본적으로 들어가있음
-// root.render(
-//   <App />
-// );
+root.render(
+  // <App />
 
-// 3장 예제
-// root.render(
-//   // <JsxUse/>
-//   <Library/>
-// );
+  // 3장 예제
+  // <JsxUse />
+  // <Library />
+
+  // 5장 예제
+  // <PropsUse />
+  // <CommentList />
+
+  // 6장 예제
+  // <NotificationList />
+  
+  // 7장 예제
+  // <Counter />
+  // <SetStateMerge />
+  // <CounterEffect />
+  // <EffectSummary />
+  // <EffectContainer/>
+  // <TimerContainer/>
+
+  // 8장 예제
+  // <Toggle/>
+  // <MyButton/>
+  // <ConfirmButton/>
+
+  // 9장 예제
+  // <Greeting isLoggedIn={true}/>
+  <LoginControl/>
+);
 
 // 4장 예제
 // 1초마다 Clock 컴포넌트를 렌더링 하는 코드
@@ -38,25 +68,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 //     <Clock1/>
 //   );
 // }, 1000);
-
-// 5장 예제
-// root.render(
-//   <PropsUse/>
-//   // <Comment/>
-//   // <CommentList/>
-// );
-
-// 6장 예제
-// root.render(
-//   <NotificationList/>
-// );
-
-// 7장 예제
-root.render(
-  // <Counter/>
-  // <SetStateMerge/>
-  <CounterEffect/>
-);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
